2f7b8a6e3c49802ed5dbda9befc389cc75e6c381
KushagraJain02 <kushagraj555@gmail.com>
2026-03-10 22:36:31 +0530

    Changes in the vite env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${import.meta.env.VITE_GATEWAY_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
@@ -20,7 +20,7 @@ axiosInstance.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default axiosInstance;
